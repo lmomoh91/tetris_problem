7895075ac855fce0f587f4496b9896e37162b5ef
lmomoh <Lmomoh@eprod.com>
2024-03-01 14:05:57 -0600

    last commit

--- tetris_problem.py
+++ tetris_problem.py
@@ -5,37 +5,24 @@ class Tetris:
     def __init__(self):
         self.board = []
         self.figures = {
-            'I': [[1, 1, 1, 1]],
-
-            'Q': [[1, 1],
-                  [1, 1]],
-
-            'T': [[1, 1, 1],
-                  [0, 1, 0]],
-
-            'Z': [[1, 1, 0],
-                  [0, 1, 1]],
-
-            'S': [[0, 1, 1],
-                  [1, 1, 0]],
-
-            'L': [[1, 0],
-                  [1, 0],
-                  [1, 1]],
-
-            'J': [[0, 1],
-                  [0, 1],
-                  [1, 1]]}
+            "I": [[1, 1, 1, 1]],
+            "Q": [[1, 1], [1, 1]],
+            "T": [[1, 1, 1], [0, 1, 0]],
+            "Z": [[1, 1, 0], [0, 1, 1]],
+            "S": [[0, 1, 1], [1, 1, 0]],
+            "L": [[1, 0], [1, 0], [1, 1]],
+            "J": [[0, 1], [0, 1], [1, 1]],
+        }
 
     def new_row(self):
         return [0 for _ in range(10)]
 
     def does_figure_fit(self, row, figure_name, pos):
         piece = self.figures.get(figure_name)
-        #iterate through figure row to determine if fits on board
+        # iterate through figure row to determine if fits on board
         for i in range(len(piece)):
             figure_row = piece[-1 * (1 + i)]
-            #check if current row is last row on board
+            # check if current row is last row on board
             if i + row == len(self.board):
                 return True
             board_row = self.board[i + row]
@@ -46,10 +33,9 @@ class Tetris:
 
     def remove_completed_rows(self, start_row, num_rows):
         # removes full rows from the board
-        # only checks rows between start_row and start_row+numRows
-        full_rows = [i + start_row
-                    for i in range(num_rows)
-                    if all(self.board[i + start_row])]
+        full_rows = [
+            i + start_row for i in range(num_rows) if all(self.board[i + start_row])
+        ]
         for completed_row in sorted(full_rows, reverse=True):
             del self.board[completed_row]
 
@@ -59,7 +45,7 @@ class Tetris:
             piece_row = piece[-1 * (1 + i)]
 
             if i + row == len(self.board):
-                self.board += self.new_row(),
+                self.board += (self.new_row(),)
             board_row = self.board[i + row]
             for j in range(len(piece_row)):
                 if piece_row[j]:
@@ -67,7 +53,7 @@ class Tetris:
         self.remove_completed_rows(row, len(piece))
 
     def add_figure(self, figure_name, pos):
-
+        #add digure to board
         blocked_by_row = None
         for row in range(len(self.board) - 1, -1, -1):
             if not self.does_figure_fit(row, figure_name, pos):
@@ -78,18 +64,19 @@ class Tetris:
         self.add_figure_at(target_row, figure_name, pos)
 
     def add_figures(self, figures: list):
-        #parse input row to get figures
-        for piece in figures.split(','):
+        # parse input row to get figures
+        for piece in figures.split(","):
             self.add_figure(piece[0], int(piece[1]))
         return len(self.board)
 
     @property
     def view_board(self):
+        #iterate the line to view board status
         for line in enumerate(self.board[::-1]):
             print(line)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import fileinput
 
     t = Tetris()
@@ -99,4 +86,3 @@ if __name__ == '__main__':
         lines = f.readlines()
     for input_row in lines:
         print(t.add_figures(input_row))
-
